9b3a28714d29c706fc26d17a9a9ff92a6ab93698
Elrond G <guocheng@shopex.cn>
2018-04-12 17:04:07 +0800

    【优化】设置子进程重启，以免内存泄漏

--- config/server.php
+++ config/server.php
@@ -6,27 +6,29 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | server config 
+    | server config
     |--------------------------------------------------------------------------
     |
     | 此处配置为swoole_serverd的配置选项, 可根据实际
+    | max_request 设置多少次执行之后，会重启子进程，防止内存泄漏
     |
     */
     'options' => [
         'user' => env('SERVER_USER'),
         'group' => env('SERVER_GROUP'),
         'daemonize' => env('SERVER_DAEMONIZE', false),
-        'worker_num' => env('SERVER_WORKER_NUM', 4)
+        'worker_num' => env('SERVER_WORKER_NUM', 4),
+        'max_request' => env('SERVER_MAX_REQUEST', 2000),
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | worker start include 
+    | worker start include
     |--------------------------------------------------------------------------
     |
     | worker 启动时需要include的文件, 默认加载路径为 bootstrap
     |
-    */    
+    */
     'worker_start_include' => [
         'route.php',
     ],
